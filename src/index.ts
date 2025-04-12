6950b9d0473d63dd109c13a3bc59bd70b4c8d2ef
foxmon <jjockrod@naver.com>
2025-04-12 18:36:52 +0900

    isDate

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export * from './packages/forEach/forEach';
 export * from './packages/clone/clone';
 export * from './packages/isFunction/isFunction';
 export * from './packages/clamp/clamp';
+export * from './packages/isDate/isDate';
