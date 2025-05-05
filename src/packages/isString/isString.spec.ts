2d8a428d9c8c1e640e8912c3a832f3736bfefdc3
foxmon <jjockrod@naver.com>
2025-05-05 16:01:19 +0900

    isString

--- src/packages/isString/isString.spec.ts
+++ src/packages/isString/isString.spec.ts
@@ -1,7 +1,7 @@
 import { isString } from './isString';
 
 describe('isString', () => {
-  test('Returns true', () => {
+  test('Returns true or false', () => {
     expect(isString('So lovely HoBom')).toBe(true);
     expect(isString([])).toBe(false);
     expect(isString(123)).toBe(false);
