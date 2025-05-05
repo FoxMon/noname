e557b1d3096ae04e678652f3a95b8870290c992c
foxmon <jjockrod@naver.com>
2025-05-05 15:58:14 +0900

    isString

--- src/packages/isString/isString.ts
+++ src/packages/isString/isString.ts
@@ -1,5 +1,12 @@
 import { NarrowedType } from '../../core/types/narrowType';
 
+/**
+ * Checks the passed value is string.
+ *
+ * @param v - The passed parameter.
+ *
+ * @category Function
+ */
 export function isString<T>(v: T | string): v is NarrowedType<T, string> {
   return typeof v === 'string';
 }
